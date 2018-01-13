7908f80c64284ab3427f5576cb26f753d5d5e8ef
Pawel Rabinovic <trueone777@googlemail.com>
2018-01-13 17:57:44 +0100

    Great new user, and alert others

--- server/server.js
+++ server/server.js
@@ -15,16 +15,34 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    //socket.emit from admin , text welcome to the chat app
+    //socket.broadcast.emit from Admin text new User joined
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app!',
+        createdAt: new Date().getTime()
+    });
 
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
 
-        socket.on('createMessage', (message) => {
-            console.log('createMessage', message);
-            io.emit('newMessage', {
-                from: message.from,
-                text: message.text,
-                createdAt: new Date().getTime()
-            });
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
+
+            // socket.broadcast.emit('newMessage', {
+            //     from: message.from,
+            //     text: message.text,
+            //     createdAt: new Date().getTime()
+            // });
         });
 
     socket.on('disconnect', () => {
